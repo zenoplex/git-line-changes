46336c51d155b6fcce14ceab8bda86cbb743f134
Gen Ichino <zenoplex@gmail.com>
2024-06-02 23:45:03 +0900

    feat: Spawn git log command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::process::Command;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to log
+    #[arg(short, long)]
+    author: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let output = Command::new("git")
+        .arg("log")
+        // .arg("--numstat")
+        // Format the output to be easily parsable
+        // https://git-scm.com/docs/pretty-formats
+        .arg("--pretty=format:%h|%aI")
+        .args(["--author", &args.author])
+        .output()
+        .expect("Failed to execute git log command");
+
+    println!(
+        "{:?}",
+        String::from_utf8(output.stdout).expect("Invalid UTF-8")
+    );
 }
