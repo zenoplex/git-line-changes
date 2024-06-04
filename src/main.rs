78917b328fa3f4c694a5f4bf5477db6da19f6de4
Gen Ichino <zenoplex@gmail.com>
2024-06-04 17:33:18 +0900

    fix: Fix month overflow

--- src/main.rs
+++ src/main.rs
@@ -109,8 +109,13 @@ fn last_day_of_year(year: i32) -> NaiveDate {
  * Get the last day of the month
  */
 fn last_day_of_month(year: i32, month: u32) -> NaiveDate {
-    let date = NaiveDate::from_ymd_opt(year, month + 1, 1).expect("Failed to create Date");
-    date.pred_opt().unwrap()
+    let date = if month == 12 {
+        NaiveDate::from_ymd_opt(year + 1, 1, 1)
+    } else {
+        NaiveDate::from_ymd_opt(year, month + 1, 1)
+    };
+
+    date.expect("Failed to create Date").pred_opt().unwrap()
 }
 
 // TODO: Needs tests
